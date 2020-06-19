2240b5c5714e59f993515ce8fad02304be057fe4
Tomas Perez <tomasitopg6@gmail.com>
2020-06-19 01:47:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -33,6 +33,10 @@ def find_the_cheese(array)# code an argument here
   
   if array.include?("cheddar")
     return "cheddar"
+  elsif array.include?("gouda")
+    return "gouda"
+  elsif array.include?("camembert")
+    return "camembert"
   else
     return false
   end
